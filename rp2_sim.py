2acb6b4f1a55f0c082a098da8958edb619198c0f
CarlosMario1818 <114821095+CarlosMario1818@users.noreply.github.com>
2022-10-02 20:01:19 -0500

    trabajo_comentario

--- rp2_sim.py
+++ rp2_sim.py
@@ -1,6 +1,16 @@
 def asm_pio(*args, **kwargs):
+'''asm_pio es un decorador que acoge argumentos y los guarda en listas
+donde *args => son argumentos que no se le asigna variables y **kawargs => son argumentos que
+se le asignan variables
+'''
     def decorador(programa):
+    '''
+    esta funcion es un decorador que recibe funciones a partir de "programa"
+    '''
         def compilador():
+        '''
+        Funcion que se ejecuta a base de la funcion recibida del decorador
+        '''
             print("Parámetros", kwargs) 
             programa()
             return None
@@ -8,7 +18,13 @@ def asm_pio(*args, **kwargs):
     return decorador
 
 def decorador_instr(fun_inst):
+'''
+Esta funcion es un decorador que recibe cierta funcion para modificar en el parametro del programa
+'''
     def decoracion_instr(self,*args, **kwargs):
+    '''
+    Esta funcion llama a la funcion (fun_inst)
+    '''
         fun_inst(self,*args, **kwargs)
         return None 
     return decoracion_instr
@@ -21,6 +37,13 @@ class PIO():
 
 class StateMachine:
   def __init__(self, id_, program, freq=125000000, **kwargs):
+  '''       
+    Es el constructor de la clase StateMachine, donde se utiliza parametro id_ a usar
+    program => que se define como el programa que se ejecutara en la clase StateMachine
+    frec => que define la frecuencia usada de en la maquina de estado
+    **kwargs => Que son los argumentos de lista. El constructor imprime las instrucciones que llegan
+    por medio de (**kwargs)
+  '''
         global sm_iniciandose,fsms
         sm_iniciandose=self
         #print('StateMachine.__init__',id_, program, freq, kwargs)
@@ -33,7 +56,10 @@ class StateMachine:
       
         
   def active(self, x=None):
-    '''Esta rutina simula exclisivamnte esa FSM. Sería interesante crear simulación en parlelo con otras FSM'''
+    '''
+    Esta rutina simula exclisivamnte esa FSM. Sería interesante
+    crear simulación en parlelo con otras FSM
+    '''
     if x==1:
         print('Está pendiente de realizar la simulacón')
 
@@ -45,6 +71,10 @@ sm_iniciandose=None
 class nop:
     @decorador_instr
     def __init__(self,*args, **kwargs):
+    '''
+    El constructor de la clase nop @decorador_instr que imprime el nombre
+    de la clase enviado por (self)
+    '''
         global sm_iniciandose
         print(self.__class__.__name__)#,'nop.__init__',args,kwargs)
         sm_iniciandose.lista_instr.append(self)
@@ -52,6 +82,10 @@ class nop:
         pass
      
     def __getitem__(self,name):
+    '''
+    Funcion que tiene un (_getitem_) y contiene el parametro
+    (self) y (name)
+    '''
         #print('nop.__getattr__',name)
         pass
         
@@ -62,11 +96,18 @@ class set(nop):
    
 class wrap_target(nop):
     def __init__(self,*args, **kwargs):
+    '''
+    Constructor de (set) que usa (unit) en la funcion de la clase
+    '''
          super().__init__(*args, **kwargs)
          pass 
   
 class wrap(nop):
     def __init__(self,*args, **kwargs):
+    '''
+    Constructor de (set) que usa (unit) en la funcion de la clase
+    '''
+
          super().__init__(*args, **kwargs)
          pass 
          
